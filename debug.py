55fb91bb7abb0c068847b94471ff6908272c4af9
ladmael <ladmael@sot01.aditsot.com>
2023-02-23 19:16:48 +0200

    pain

--- debug.py
+++ debug.py
@@ -1,3 +1,4 @@
+
 import gnupg
 import os
 import smtplib
@@ -7,30 +8,20 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
 def send_encrypted_message(gpg, recipient_email, message, subject, sender_email, sender_password):
-    # создаем сообщение с несколькими частями
+    # шифруем сообщение
+    ciphertext = str(gpg.encrypt(message.encode('utf-8'), recipient_email.encode('utf-8'), always_trust=True))
+
+    # создаем сообщение с зашифрованным текстом
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender_email
     msg['To'] = recipient_email
+    msg.attach(MIMEText(ciphertext))
+    
+    img = MIMEImage(open('1.jpg', 'rb').read())
+    img.add_header('Content-Disposition', 'attachment', filename='1.jpg.asc')
+    msg.attach(img)
 
-    # добавляем текстовую часть сообщения
-    text = MIMEText(message, 'plain', 'utf-8')
-    msg.attach(text)
-
-    # добавляем изображения
-    with open('1.jpg', 'rb') as f:
-        img = MIMEImage(f.read())
-        msg.attach(img)
-
-    # шифруем сообщение
-    ciphertext = str(gpg.encrypt(msg.as_bytes(), recipient_email.encode('utf-8'), always_trust=True))
-
-    # создаем MIME-часть для зашифрованного сообщения
-    ciphertext_part = MIMEText(ciphertext, 'plain', 'utf-8')
-    ciphertext_part.set_charset('utf-8')
-    ciphertext_part.replace_header('Content-Disposition', 'inline')
-    ciphertext_part.replace_header('Content-Transfer-Encoding', 'quoted-printable')
-    msg.attach(ciphertext_part)
 
     try:
         with smtplib.SMTP('mail.syneforge.com', 587) as server:
@@ -40,6 +31,8 @@ def send_encrypted_message(gpg, recipient_email, message, subject, sender_email,
             print("Отправлено " + recipient_email)
     except: 
         print("Не отправлено " + recipient_email)
+
+
 
 gpg = gnupg.GPG(gnupghome="./gpg")
 
@@ -52,10 +45,12 @@ subject = input("Введите тему сообщения: ")
 sender_email = input("Введите свою почту (email@syneforge.com): ")
 sender_password = getpass.getpass("Введите свой пароль от почты: ")
 
+id = 0
+
 for key in imported_keys.results:
     fingerprint = key['fingerprint']
-    email = gpg.list_keys()[i]["uids"][0]
-    i=i + 1
+    email = gpg.list_keys()[id]["uids"][0]
+    id = id + 1
     send_encrypted_message(gpg, email, plaintext, subject, sender_email, sender_password)
 
 print()
